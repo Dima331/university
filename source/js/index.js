7fce8df70a9a9c7d658ad6055e397a66c8e1481f
D <Di_1@inbox.ru>
2022-11-22 14:34:28 +0300

    feat: add section partners

--- source/js/index.js
+++ source/js/index.js
@@ -2,10 +2,10 @@ import Swiper, { Navigation, Pagination, Autoplay, Breakpoints, Keyboard } from 
 
 Swiper.use([Navigation, Pagination, Autoplay, Breakpoints, Keyboard]);
 
-const resources = new Swiper('.slider__container', {
+const resources = new Swiper('.resources__slider', {
   navigation: {
-    nextEl: '.slider__button-next',
-    prevEl: '.slider__button-prev'
+    nextEl: '.resources__next',
+    prevEl: '.resources__prev'
   },
   keyboard: {
     enabled: true,
@@ -33,6 +33,43 @@ const resources = new Swiper('.slider__container', {
     },
     "@1.50": {
       slidesPerView: 7,
+      spaceBetween: 20
+    }
+  },
+  loop: true,
+});
+
+const partners = new Swiper('.partners__slider', {
+  navigation: {
+    nextEl: '.partners__next',
+    prevEl: '.partners__prev'
+  },
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    400: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    "@0.50": {
+      slidesPerView: 4,
+      spaceBetween: 20
+    },
+    "@0.75": {
+      slidesPerView: 5,
+      spaceBetween: 20
+    },
+    "@1.00": {
+      slidesPerView: 6,
+      spaceBetween: 20
+    },
+    "@1.50": {
+      slidesPerView: 6,
       spaceBetween: 20
     }
   },
