f7271556e6b2f2d6dbc100f31a13c7a4db18eecf
D <Di_1@inbox.ru>
2022-11-24 10:11:13 +0300

    feat: add section banners

--- source/js/index.js
+++ source/js/index.js
@@ -75,3 +75,58 @@ const partners = new Swiper('.partners__slider', {
   },
   loop: true,
 });
+
+const banners = new Swiper('.banners__slider', {
+  navigation: {
+    nextEl: '.banners__next',
+    prevEl: '.banners__prev'
+  },
+  keyboard: {
+    enabled: false,
+    onlyInViewport: false,
+  },
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true
+  },
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  grabCursor: true,
+  loopedSlides: "3",
+  loop: true,
+});
+
+document.addEventListener('mouseenter', event => {
+  const el = event.target;
+  if (el && el.matches && el.matches('.banners__slider')) {
+    el.swiper.autoplay.stop();
+    el.classList.add('swiper-paused');
+
+    const activeNavItem = el.querySelector('.swiper-pagination-bullet-active');
+    if (activeNavItem) {
+      activeNavItem.style.animationPlayState = "paused";
+    }
+  }
+}, true);
+
+document.addEventListener('mouseleave', event => {
+  const el = event.target;
+  if (el && el.matches && el.matches('.banners__slider')) {
+    el.swiper.autoplay.start();
+    el.classList.remove('swiper-paused');
+
+    const activeNavItem = el.querySelector('.swiper-pagination-bullet-active');
+
+    activeNavItem.classList.remove('swiper-pagination-bullet-active');
+
+    setTimeout(() => {
+      activeNavItem.classList.add('swiper-pagination-bullet-active');
+    }, 10);
+  }
+}, true);
